540f59e7c95b80aabb1c568ab0ee9b7499aeb7c5
Zelo-S <stevethesteve990@gmail.com>
2025-02-22 17:06:20 -0600

    Graph for direct connection to ODRIVE

--- odri_dog/graph.py
+++ odri_dog/graph.py
@@ -3,10 +3,11 @@ import odrive
 import time
 from live_plotter import LivePlotter
 import matplotlib.pyplot as plt
+from threading import Thread
 
 from live_plotter import FastLivePlotter
-live_plotter = FastLivePlotter(legends=[["point", "setpoint"]], ylims=[(-0.5, 0)])
-live_plotter2 = FastLivePlotter(legends=[["current", "current"]], ylims=[(-8,8)])
+live_plotter = FastLivePlotter(legends=[["point", "setpoint"]], ylims=[(-1.5, 1.5)])
+# live_plotter2 = FastLivePlotter(legends=[["current", "current"]], ylims=[(-9, 9)])
 odrv0 = odrive.find_any()
 
 def plot():
@@ -16,25 +17,31 @@ def plot():
 
     while True:
         # Retrieve data from the ODrive
-        pos = odrv0.axis0.encoder.pos_estimate
-        setpoint = odrv0.axis0.controller.pos_setpoint
-        current = odrv0.axis0.motor.current_control.Iq_measured
+        try:
+            pos = odrv0.axis0.encoder.pos_estimate
+            setpoint = odrv0.axis0.controller.pos_setpoint
+            current = odrv0.axis0.motor.current_control.Iq_measured
+        finally:
+            pos = 0
+            setpoint = 0
+            current = 0
         
         pos_list.append(pos)
         setpoint_list.append(setpoint)
         current_list.append(current)
 
         y_data_1 = np.stack([np.array(pos_list), np.array(setpoint_list)], axis=1)
-        y_data_2 = np.stack([np.array(current_list), np.array(current_list)], axis=1)
+        # y_data_2 = np.stack([np.array(current_list), np.array(current_list)], axis=1)
 
         live_plotter.plot(
             y_data_list=[y_data_1]
         )
-        live_plotter2.plot(
-            y_data_list=[y_data_2]
-        )
+        # live_plotter2.plot(
+            # y_data_list=[y_data_2]
+        # )
 
         # Control the update rate
         time.sleep(0.01)  # Adjust the sleep time as needed
     
-plot()
+plot_thread = Thread(target=plot)
+plot_thread.start()
